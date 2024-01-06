644c318789e4891308c084651554a1a1d127b985
Jae Sim <jaegoo@colorless.net>
2024-01-06 08:09:16 -0800

    Adding CardView

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,32 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Jae Sim on 1/6/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Label("\(scrum.lengthInMinutes)"/*@END_MENU_TOKEN@*/, systemImage: /*@START_MENU_TOKEN@*/"clock")
+            }
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -33,14 +33,20 @@ struct MeetingView: View {
 
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            
             Circle()
                 .strokeBorder(lineWidth: 24)
+            
             HStack {
                 Text("Speaker 1 of 3")
                 Spacer()
                 Button(action: {}){
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel("Next speaker")
             }
         }.padding()
     }
